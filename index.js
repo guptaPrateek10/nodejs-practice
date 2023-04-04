156acd1df9cce2f313cd1b00adf6e7e7c20d6102
guptaPrateek10 <guptaPrateek10@gmail.com>
2023-04-04 13:12:08 +0530

    Mongo Db schema define

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import http from "http";
 import path from "path";
 import express from "express";
 import fs from "fs";
+import mongoose from "mongoose";
 //it tells us the directopry name. path has some other methods as well.
 // console.log(path.dirname("/home/index,js"));
 
@@ -18,26 +19,66 @@ import fs from "fs";
 // server.listen(5000, () => {
 //   console.log("server is running");
 // });
+const users = [];
 
 //this is express framework approch
 const app = express();
-
-app.get("/getFile", (req, res) => {
-  //   res.sendFile("./index.html");
-  const pathloc = path.resolve();
-  res.sendFile(path.join(pathloc, "./index.html"));
-  //   const file = fs.readFileSync("./index.html");
-});
 
 app.listen(5000, () => {
   console.log("server has started");
 });
 
-//using middlewares.
+//using  middlewares.
+// to make the public folder as default
 app.use(express.static(path.join(path.resolve(), "public")));
+
+//This is a by default middleware for POST and PUT req for sending the req.body to the server
+app.use(express.urlencoded({ extended: true }));
 
 //setting up View engine
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { name: "prateek" });
+  // res.sendFile("index");
+});
+app.get("/success", (req, res) => {
+  res.render("success");
+  // res.sendFile("index");
+});
+app.post("/contactsave", (req, res) => {
+  users.push({ user_name: req.body.name, email: req.body.email });
+  // res.render("success");
+  res.redirect("/success");
+  console.log(users[0].email);
+});
+
+app.get("/users", (req, res) => {
+  res.json({ users });
+});
+
+// app.get("/getFile", (req, res) => {
+//   //   res.sendFile("./index.html");
+//   const pathloc = path.resolve();
+//   res.sendFile(path.join(pathloc, "./index.html"));
+//   //   const file = fs.readFileSync("./index.html");
+// });
+
+// app.get("/", (req, res) => {
+// res.json({
+//   name: "Prateek",
+//   product: [{ Gupta: "21" }],
+// });
+// res.status(400).json("meri merzi");
+// });
+
+// app.get("/getFile", (req, res) => {
+//   const pathlocation = path.resolve();
+//   const file = fs.readFileSync(path.join(pathlocation, "./index.html"));
+//   // res.send("./index.html");
+//   res.sendFile(path.join(pathlocation, "./index.html"));
+// });
+
 // app.get("/", (req, res) => {
 //   //   res.send("Home Page");
 //   //   res.sendStatus(404);
@@ -50,3 +91,36 @@ app.set("view engine", "ejs");
 //   res.status(400).send("Merimerzi");
 // });
 //
+
+//Mongo db uses mongoose as a client for schema
+mongoose
+  .connect("mongodb://localhost:27017", {
+    dbName: "backend",
+  })
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//to create the mongo db schema
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+//to create mongodb table(called model)
+const message = mongoose.model("usertables", messageSchema);
+
+app.post("/contact", (req, res) => {
+  const { name, email } = req.body;
+  message
+    .create({
+      name: name,
+      email: email,
+    })
+    .then(() => {
+      res.send("User Data Added");
+    });
+});
